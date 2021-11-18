875ead41e8a193173e239570ac2a023c867b1966
Tharre <tharre3@gmail.com>
2021-11-18 16:26:55 +0100

    More stuff

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 cur_pkgver() {
 	parse_srcinfo --json < .SRCINFO | jq -r '(if .epoch == null then "" else
@@ -8,9 +8,9 @@ cur_pkgver() {
 
 for dir in $(aur graph */.SRCINFO | tsort | tac); do
 	pushd "$dir" > /dev/null
-	if [ $(vercmp $(join -o 1.2,2.2 -a2 -e '0' <(aur repo -Sld sr.ht) <(cur_pkgver) | head -n1)) -lt 0 ]; then
-		echo "=== Building $dir ===\n"
-		aur build -c -r /home/build/local -d localrepo -- --noprogressbar
-	fi
+	# if [ $(vercmp $(join -o 1.2,2.2 -a2 -e '0' <(aur repo -Sld sr.ht) <(cur_pkgver) | head -n1)) -lt 0 ]; then
+	# 	echo "=== Building $dir ===\n"
+	aur build -c -r /home/build/local -d localrepo -- --noprogressbar
+	# fi
 	popd > /dev/null
 done
