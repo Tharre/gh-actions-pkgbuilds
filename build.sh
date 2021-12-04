cc0f156e1f15e7ac9351c986d9dd619f9c6ba041
Tharre <tharre3@gmail.com>
2021-12-04 04:01:41 +0100

    Testing71

--- build.sh
+++ build.sh
@@ -1,16 +1,24 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-cur_pkgver() {
-	parse_srcinfo --json < .SRCINFO | jq -r '(if .epoch == null then "" else
-			.epoch + ":" end + .pkgver + "-" + .pkgrel) as $version
-			| .packages | to_entries[] | [.key, $version] | @tsv' | sort
-}
+CHROOT=/buildchroot
 
-for dir in $(aur graph */.SRCINFO | tsort | tac); do
+for dir in $(aur-graph */.SRCINFO | tsort | tac); do
+	echo testing "$dir"
+
 	pushd "$dir" > /dev/null
-	# if [ $(vercmp $(join -o 1.2,2.2 -a2 -e '0' <(aur repo -Sld sr.ht) <(cur_pkgver) | head -n1)) -lt 0 ]; then
-	# 	echo "=== Building $dir ===\n"
-	aur build -c -r /home/build/local -d localrepo -- --noprogressbar
-	# fi
+
+	# directory may also reference the pkgbase, in which case test if the
+	# first package in pkgname is up to date
+	remotever="$(expac -S1 "%v" "custom/$dir" || expac -S1 "%v" "custom/$(source PKGBUILD; printf %s "$pkgname")" || echo NONE)"
+	echo DEBUG: $remotever
+
+	# TODO: there should be EPOCH in here somewhere
+	if [ $(vercmp "$remotever" $(source PKGBUILD; printf %s "$pkgver-$pkgrel")) -lt 0 ]; then
+		echo "=== Building $dir ==="
+		makechrootpkg -c -u -U build -D /repository -r "$CHROOT"
+		repo-add /repository/custom.db.tar.gz *.pkg.tar*
+		mv *.pkg.tar* /repository/
+		#aur build -c -r /home/build/custom -d custom -- --noprogressbar
+	fi
 	popd > /dev/null
 done
